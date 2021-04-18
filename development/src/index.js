8928836812daa260980f29c90c2a979d5be2b6a5
shivank goyal <goyal.shivank790@gmail.com>
2021-04-18 19:42:58 +0530

    initial commit

---
+++ development/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import Tictactoe from "E:/react_practice/development/src/Tic-tac-toe/Tic-tac-toe.js";
+ReactDOM.render(<Tictactoe/>,
+  document.getElementById('root')
+);
+
+
